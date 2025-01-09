a851f42e2a7b4ef8cecf210a3b1427ae9ebed2a8
Akin Duran <akindur@gmail.com>
2025-01-09 09:31:46 +0100

    Adding the DB connection and Scalar

---
+++ PortfolioAkinApi/Controllers/HomeController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using PortfolioAkinApi.Data;
+
+
+namespace PortfolioAkinApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class HomeController : ControllerBase
+    {
+        private readonly ApplicationDbContext _context;
+
+        public HomeController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+    }
+}
--- PortfolioAkinApi/Program.cs
+++ PortfolioAkinApi/Program.cs
@@ -1,4 +1,8 @@
 
+using Microsoft.EntityFrameworkCore;
+using PortfolioAkinApi.Data;
+using Scalar.AspNetCore;
+
 namespace PortfolioAkinApi
 {
     public class Program
@@ -8,7 +12,7 @@ namespace PortfolioAkinApi
             var builder = WebApplication.CreateBuilder(args);
 
             // Add services to the container.
-
+            builder.Services.AddDbContext<ApplicationDbContext>(options => options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
             builder.Services.AddControllers();
             // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
             builder.Services.AddOpenApi();
@@ -19,6 +23,7 @@ namespace PortfolioAkinApi
             if (app.Environment.IsDevelopment())
             {
                 app.MapOpenApi();
+                app.MapScalarApiReference();
             }
 
             app.UseHttpsRedirection();
